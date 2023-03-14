bfb63751bdb16f83da17a644a961bc15e2774a6a
Danushka Pathirana <pathirana.danushka.rp@gmail.com>
2023-03-14 19:34:39 +0530

    refactor the code which sending cart data http request from inside the slice using an action creator thunk

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,3 +1,5 @@
+import { uiActions } from "./ui-slice";
+
 const { createSlice } = require("@reduxjs/toolkit");
 
 const cartSlice = createSlice({
@@ -31,6 +33,32 @@ const cartSlice = createSlice({
         }
     }
 })
+
+// create a action creator (action creator thunk)
+export const sendCartData = (cart) => {
+    return async (dispatch) => {
+
+        dispatch(uiActions.showNotification({status: 'pending', title: 'Sending...', message: 'Sending cart data!'}))
+
+        const sendRequest = async () => {
+            const response = await fetch(
+                'https://shopping-cart-app-b19c5-default-rtdb.firebaseio.com/cart.json', 
+                {method: 'PUT', body: JSON.stringify(cart)
+            })
+    
+            if(!response.ok) {
+                throw new Error('Sending cart data is failed!')
+            }
+        }
+        try {
+            await sendRequest()
+            dispatch(uiActions.showNotification({status: 'success', title: 'Success', message: 'Send cart data successfully!'}))
+        }
+        catch(err) {
+            dispatch(uiActions.showNotification({status: 'error', title: 'Error', message: 'Sending cart data failed!'}))
+        }
+    }
+}
 
 export const cartActions = cartSlice.actions
 export default cartSlice
