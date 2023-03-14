496d4a62d0c49507a38dc9b2d0a333ada41669d0
Danushka Pathirana <pathirana.danushka.rp@gmail.com>
2023-03-14 17:39:39 +0530

    develop toggling cart ui

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { uiActions } from '../../store/ui-slice';
 
 import classes from '../Cart/CartButton.module.css'
 
 const CartButton = (props) => {
+  const dispatch = useDispatch()
+
+  const toggleCartHandler = () => { 
+    dispatch(uiActions.toggle())
+  }
+
   return(
-    <button className={classes.button}>
+    <button className={classes.button} onClick={toggleCartHandler}>
       <span>My Cart</span>
       <span className={classes.badge}>1</span>
     </button>
