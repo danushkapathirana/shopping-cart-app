ff92579db0bd929d51634027c6e01b1a3ce9f1da
Danushka Pathirana <pathirana.danushka.rp@gmail.com>
2023-03-14 17:41:46 +0530

    develop updating cart ui and cart badge

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -4,12 +4,17 @@ import ProductItem from "./ProductItem";
 
 import classes from '../Shop/Products.module.css'
 
+const DUMMY_PRODUCTS = [
+  {id: 'p1', price: 6, title: 'Banana', description: 'A banana is an elongated, edible fruit'},
+  {id: 'p2', price: 10, title: 'Mango', description: 'A mango is an edible stone fruit'},
+]
+
 const Products = (props) => {
   return(
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem title='Test' price={6} description='This is a first product - amazing!' />
+        {DUMMY_PRODUCTS.map((product) => <ProductItem key={product.id} id={product.id} title={product.title} price={product.price} description={product.description} />)}
       </ul>
     </section>
   )
